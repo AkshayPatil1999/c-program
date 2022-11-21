b00395f7f7ecbefa6739717702c1d43c09bfe2ad
AkshayPatil1999 <118713832+AkshayPatil1999@users.noreply.github.com>
2022-11-21 12:31:20 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+  int1,fact = 1,n;
+  printf("\n Enter n = ");
+  scanf("%d",&n)
+    for(i=1;i<=n;i++)
+    {
+      fact = fact*i
+      }
+  printf("\n fact=%d",fact);
+}
